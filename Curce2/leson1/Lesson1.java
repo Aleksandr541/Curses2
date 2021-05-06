654fff21d82a22e2307f39ebba92eb071f4e247b
Alexandr541 <adshvec@gmail.com>
2021-05-06 15:32:44 +0300

    lesson1

---
+++ Curce2/leson1/Lesson1.java
@@ -0,0 +1,10 @@
+package leson1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 10; i >0; i--) {
+            System.out.println(i);
+        }
+    }
+}
